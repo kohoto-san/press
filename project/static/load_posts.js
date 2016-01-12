51fc49c6d8acd5e458525eb75b987123cd093fa4
Michael <kohotosan@gmail.com>
2016-01-12 14:35:28 +1000

    change load posts in home

--- project/static/load_posts.js
+++ project/static/load_posts.js
@@ -1,45 +1,62 @@
 var page = 1;
 var first_load = false;
+var is_loading = false;
 
 $( window ).load(function() {
+    loading('off');
     loadPosts();
 });
 
 $('#load_posts').click(function(){
-    loadPosts();
+    if( !is_loading ){
+        loading('off');
+        loadPosts();
+    }
 });
 
 
+function loading(turn){
+    // Когда посты загружены включить возможность загрузить ещё
+    if(turn == 'on'){
+        $('#load_posts a').removeClass('disabled');
+        is_loading = false;
+    }
+    // Когда посты начали грузиться выключить возможность загрузить ещё
+    else{
+        $('#load_posts a').addClass('disabled');
+        is_loading = true;
+    }
+
+}
+
+
 function loadPosts(){
-            
-    if( $(this).find('a').hasClass("disabled") == false){
 
-        $.get( window.location.href, {page: page} )
+    $.get( window.location.href, {page: page} )
                 
-            .success(function( data ) {
+        .success(function( data ) {
 
-                var newElems = $( data );
-                var $container = $('#content-posts-wrapper');
+            var newElems = $( data );
+            var $container = $('#content-posts-wrapper');
 
-                $container.masonryImagesReveal( newElems );
+            $container.masonryImagesReveal( newElems );
 
-                /*
-                if ($container.data("masonry")) {
-                    $container.append( newElems ).masonry( "appended", newElems );
-                }
-                else {
-                    $container.append( data );
-                }
-                */
+            /*
+            if ($container.data("masonry")) {
+                $container.append( newElems ).masonry( "appended", newElems );
+            }
+            else {
+                $container.append( data );
+            }
+            */
 
-            }) // success || done
+        }) // success || done
                 
-            .fail(function() {
-                $('#load_posts').prepend('<p>Упс...Кажется все посты уже загружены.</p>')
-                $('#load_posts a').addClass('disabled');
-            }); // fail
-
-    }    // hasClass
+        .fail(function() {
+            $('#load_posts').prepend('<p>Упс...Кажется все посты уже загружены.</p>')
+            $('#load_posts a').addClass('disabled');
+        }); // fail
+    
 
 } // function loadPosts
 
@@ -69,11 +86,19 @@ function loadPosts(){
                 }
 
             })
-            .always( function( instance ) {                
+            .always( function( instance ) {
                 page += 1;
+                loading('on');
             });
   
         return this;
     };
 
 })( jQuery );
+
+// обрабатываем незагруженные изображения
+$(function(){
+    $('img').error(function(){
+        $(this).attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+    });
+});
