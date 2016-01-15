26ace68bb25f66ba26889c939ccefd2e0335e1b4
Michael <kohotosan@gmail.com>
2016-01-15 15:57:45 +1000

    add content quiz from file

--- project/quiz/models.py
+++ project/quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import yaml
 
 
 # Company / Startuper
@@ -32,9 +33,22 @@ class TextContent(models.Model):
 
     content = models.FileField(upload_to='files', null=True)
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            stream = self.content.read()
+            text_list = yaml.load(stream)
+
+            for company in text_list:
+                facts = text_list[company]
+                answer = Answer.objects.create(name=company)
+                for fact in facts:
+                    Fact.objects.create(text=fact, answer=answer)
+
+        return super(TextContent, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name = "TextContent"
         verbose_name_plural = "TextContents"
 
     def __str__(self):
-        return self.content
+        return self.content.url
